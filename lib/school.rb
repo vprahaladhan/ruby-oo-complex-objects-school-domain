866548c845aa7e5023b26a68010e0e5ebd189ce4
prahal <vprahaladhan@gmail.com>
2021-05-06 20:59:05 +1000

    commit tree 0af2ada93fd49db5047d43b8d61a0f2ca79d0622

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School 
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def name 
+    @name
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = @roster[grade] ? @roster[grade] << name : [] << name  
+  end  
+
+  def grade(input_grade)
+    @roster[input_grade]
+  end  
+
+  def sort
+    @roster.each do |key, value|
+      @roster[key] = value.sort
+    end
+    @roster
+  end
+end
